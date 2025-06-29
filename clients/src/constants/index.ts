6f270e6c8368da6b5119b3953c23cfc24c84690a
Sabbir Hossain Shuvo <82939905+devlopersabbir@users.noreply.github.com>
2025-06-29 13:22:45 +0600

    Update index.ts

--- clients/src/constants/index.ts
+++ clients/src/constants/index.ts
@@ -1,2 +1,2 @@
 // export const baseUri = "https://145.223.97.55:8443" as const;
-export const baseUri = "https://145-223-97-55.nip.io:8443" as const;
+export const baseUri = "https://145.223.97.55:6767" as const;
